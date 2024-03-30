4d99c83b55a73be011109a062209ea6189a8ada5
ooosuke <oosuke@dissmo.biz>
2024-03-31 00:52:02 +0900

    feat: add chakura ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import { ChakraProvider } from '@chakra-ui/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +15,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ja">
+      <body>
+        <ChakraProvider>
+          {children}
+        </ChakraProvider>
+      </body>
     </html>
   );
 }
